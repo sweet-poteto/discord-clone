d4124dbca0c4ae2000f04267d2e2f443ef86b4f8
sweet-poteto <142521579+sweet-poteto@users.noreply.github.com>
2023-08-26 20:02:57 +0530

    dark and light theme setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 export default function HomePage() {
   return (
     <div className="">
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
