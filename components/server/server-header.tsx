f6793772626685f2930e8a4b201e393914c7afb2
sweet-poteto <142521579+sweet-poteto@users.noreply.github.com>
2023-09-09 12:28:03 +0530

    add invite functionality

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -19,6 +19,7 @@ import {
   DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
 import { ServerWithMemberWithProfile } from "@/types";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerHeaderProps {
   server: ServerWithMemberWithProfile;
@@ -26,6 +27,7 @@ interface ServerHeaderProps {
 }
 
 export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  const { onOpen } = useModal();
   const isAdmin = (role = MemberRole.ADMIN);
   const isModerator = isAdmin || role === MemberRole.MODERATOR;
 
@@ -39,7 +41,10 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
         {isModerator && (
-          <DropdownMenuItem className="px-3 py-2 text-sm text-indigo-600 cursor-pointer dark:text-indigo-400">
+          <DropdownMenuItem
+            className="px-3 py-2 text-sm text-indigo-600 cursor-pointer dark:text-indigo-400"
+            onClick={() => onOpen("invite", { server })}
+          >
             Invite People
             <UserPlus className="w-4 h-4 ml-auto" />
           </DropdownMenuItem>
