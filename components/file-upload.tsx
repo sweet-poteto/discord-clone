5ccd524ff56810356ffb7a1c5e2942af4175bd88
sweet-poteto <sagarpardhi.dev@outlook.com>
2023-10-17 12:31:09 +0530

    add message attachment

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 
 import { UploadDropzone } from "@/lib/uploadthing";
@@ -18,14 +18,37 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
 
   if (value && fileType !== "pdf") {
     return (
-      <div className="relative h-20 w-20">
+      <div className="relative w-20 h-20">
         <Image fill src={value} alt="upload" className="rounded-full" />
         <button
           onChange={() => onChange("")}
-          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm "
+          className="absolute top-0 right-0 p-1 text-white rounded-full shadow-sm bg-rose-500 "
           type="button"
         >
-          <X className="h-4 w-4" />
+          <X className="w-4 h-4" />
+        </button>
+      </div>
+    );
+  }
+
+  if (value && fileType === "pdf") {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="w-10 h-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+        <button
+          onChange={() => onChange("")}
+          className="absolute p-1 text-white rounded-full shadow-sm -top-2 -right-2 bg-rose-500 "
+          type="button"
+        >
+          <X className="w-4 h-4" />
         </button>
       </div>
     );
