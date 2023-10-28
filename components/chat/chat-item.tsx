1e24a0e6e38f3af7949b1c91b04c8516c7919751
sweet-poteto <sagarpardhi.dev@outlook.com>
2023-10-28 11:58:01 +0530

    add use chat hook

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -24,6 +24,7 @@ import { Form, FormControl, FormField, FormItem } from "../ui/form";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { useModal } from "@/hooks/use-modal-store";
+import { useParams, useRouter } from "next/navigation";
 
 interface ChatItemProps {
   id: string;
@@ -64,6 +65,15 @@ export const ChatItem = ({
 }: ChatItemProps) => {
   const [isEditing, setIsEditing] = useState(false);
   const { onOpen } = useModal();
+  const params = useParams()
+  const router = useRouter()
+
+  const onMemberClick = () => {
+    if (member.id === currentMember.id) {
+      return
+    }
+    router.push(`/servers/${params?.serverId}/conversations/${member.id}`)
+  }
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -121,13 +131,13 @@ export const ChatItem = ({
   return (
     <div className="relative flex items-center w-full p-4 transition group hover:bg-black/5">
       <div className="flex items-start w-full group gap-x-2">
-        <div className="transition cursor-pointer hover:drop-shadow-md">
+        <div className="transition cursor-pointer hover:drop-shadow-md" onClick={onMemberClick}>
           <UserAvatar src={member.profile.imageUrl} />
         </div>
         <div className="flex flex-col w-full">
           <div className="flex items-center gap-x-2">
             <div className="flex items-center">
-              <p className="text-sm font-semibold cursor-pointer hover:underline">
+              <p className="text-sm font-semibold cursor-pointer hover:underline" onClick={onMemberClick}>
                 {member.profile.name}
               </p>
               <ActionTooltip label={member.role}>
--- components/chat/chat-messages.tsx
+++ components/chat/chat-messages.tsx
@@ -8,6 +8,7 @@ import { format } from "date-fns";
 import { useChatQuery } from "@/hooks/use-chat-query";
 import { ChatWelcome } from "./chat-welcome";
 import { ChatItem } from "./chat-item";
+import { useChatSocket } from "@/hooks/use-chat-socket";
 
 const DATE_FORMAT = "d MMM yyyy, HH:MM";
 
@@ -40,6 +41,8 @@ export const ChatMessages = ({
   type,
 }: ChatMessagesProps) => {
   const queryKey = `chat:${chatId}`;
+  const addKey = `chat:${chatId}:messages`;
+  const updateKey = `chat:${chatId}:messages:update`;
 
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, status } =
     useChatQuery({
@@ -48,6 +51,8 @@ export const ChatMessages = ({
       paramKey,
       paramValue,
     });
+
+  useChatSocket({ queryKey, addKey, updateKey });
 
   if (status === "loading") {
     return (
