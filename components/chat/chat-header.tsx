e7b49c412f3f17fc930342654d95cd053bf6059f
sweet-poteto <sagarpardhi.dev@outlook.com>
2023-11-14 13:35:15 +0530

    add audio and video calls

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -3,6 +3,7 @@ import { Hash, Menu } from "lucide-react";
 import { MobileToggle } from "@/components/mobile-toggle";
 import { UserAvatar } from "@/components/user-avatar";
 import { SocketIndicator } from "@/components/socket-indicator";
+import { ChatVideoButton } from "./chat-video-button";
 
 interface ChatHeaderProps {
   serverId: string;
@@ -28,6 +29,7 @@ export const ChatHeader = ({
       )}
       <p className="font-semibold text-black text-md dark:text-white">{name}</p>
       <div className="flex items-center ml-auto">
+        {type === "conversation" && <ChatVideoButton />}
         <SocketIndicator />
       </div>
     </div>
